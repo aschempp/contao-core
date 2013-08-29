a732aef9c8212e175f8c0fe6a4b13f8b43360e71
Andreas Schempp <andreas.schempp@terminal42.ch>
2013-08-29 15:00:15 +0200

    We don't need a separate method, all cases have an exit statement

--- system/modules/core/classes/Ajax.php
+++ system/modules/core/classes/Ajax.php
@@ -206,209 +206,199 @@ class Ajax extends \Backend
 		header('Content-Type: text/html; charset=' . $GLOBALS['TL_CONFIG']['characterSet']);
 
 		// Only execute the logic below for the "stock drivers" - allow the HOOK usage for third party drivers.
-		if (!(($dc instanceof \DC_File) || ($dc instanceof \DC_Folder) || ($dc instanceof \DC_Table)))
+		if ($dc instanceof \DC_File || $dc instanceof \DC_Folder || $dc instanceof \DC_Table)
 		{
-			$this->executePostActionsHOOK($dc);
-		}
+			switch ($this->strAction)
+			{
+				// Load nodes of the page structure tree
+				case 'loadStructure':
+					echo $dc->ajaxTreeView($this->strAjaxId, intval(\Input::post('level')));
+					exit; break;
 
-		switch ($this->strAction)
-		{
-			// Load nodes of the page structure tree
-			case 'loadStructure':
-				echo $dc->ajaxTreeView($this->strAjaxId, intval(\Input::post('level')));
-				exit; break;
+				// Load nodes of the file manager tree
+				case 'loadFileManager':
+					echo $dc->ajaxTreeView(\Input::post('folder', true), intval(\Input::post('level')));
+					exit; break;
 
-			// Load nodes of the file manager tree
-			case 'loadFileManager':
-				echo $dc->ajaxTreeView(\Input::post('folder', true), intval(\Input::post('level')));
-				exit; break;
+				// Load nodes of the page tree
+				case 'loadPagetree':
+					$arrData['strTable'] = $dc->table;
+					$arrData['id'] = $this->strAjaxName ?: $dc->id;
+					$arrData['name'] = \Input::post('name');
 
-			// Load nodes of the page tree
-			case 'loadPagetree':
-				$arrData['strTable'] = $dc->table;
-				$arrData['id'] = $this->strAjaxName ?: $dc->id;
-				$arrData['name'] = \Input::post('name');
+					$objWidget = new $GLOBALS['BE_FFL']['pageSelector']($arrData, $dc);
+					echo $objWidget->generateAjax($this->strAjaxId, \Input::post('field'), intval(\Input::post('level')));
+					exit; break;
 
-				$objWidget = new $GLOBALS['BE_FFL']['pageSelector']($arrData, $dc);
-				echo $objWidget->generateAjax($this->strAjaxId, \Input::post('field'), intval(\Input::post('level')));
-				exit; break;
+				// Load nodes of the file tree
+				case 'loadFiletree':
+					$arrData['strTable'] = $dc->table;
+					$arrData['id'] = $this->strAjaxName ?: $dc->id;
+					$arrData['name'] = \Input::post('name');
 
-			// Load nodes of the file tree
-			case 'loadFiletree':
-				$arrData['strTable'] = $dc->table;
-				$arrData['id'] = $this->strAjaxName ?: $dc->id;
-				$arrData['name'] = \Input::post('name');
+					$objWidget = new $GLOBALS['BE_FFL']['fileSelector']($arrData, $dc);
 
-				$objWidget = new $GLOBALS['BE_FFL']['fileSelector']($arrData, $dc);
-
-				// Load a particular node
-				if (\Input::post('folder', true) != '')
-				{
-					echo $objWidget->generateAjax(\Input::post('folder', true), \Input::post('field'), intval(\Input::post('level')));
-				}
-				else
-				{
-					echo $objWidget->generate();
-				}
-				exit; break;
-
-			// Reload the page/file picker
-			case 'reloadPagetree':
-			case 'reloadFiletree':
-				$intId = \Input::get('id');
-				$strField = $strFieldName = \Input::post('name');
-
-				// Handle the keys in "edit multiple" mode
-				if (\Input::get('act') == 'editAll')
-				{
-					$intId = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', $strField);
-					$strField = preg_replace('/(.*)_[0-9a-zA-Z]+$/', '$1', $strField);
-				}
-
-				// Validate the request data
-				if ($GLOBALS['TL_DCA'][$dc->table]['config']['dataContainer'] == 'File')
-				{
-					// The field does not exist
-					if (!array_key_exists($strField, $GLOBALS['TL_CONFIG']))
+					// Load a particular node
+					if (\Input::post('folder', true) != '')
 					{
-						$this->log('Field "' . $strField . '" does not exist in the global configuration', 'Ajax executePostActions()', TL_ERROR);
-						header('HTTP/1.1 400 Bad Request');
-						die('Bad Request');
-					}
-				}
-				elseif ($this->Database->tableExists($dc->table))
-				{
-					// The field does not exist
-					if (!$this->Database->fieldExists($strField, $dc->table))
-					{
-						$this->log('Field "' . $strField . '" does not exist in table "' . $dc->table . '"', 'Ajax executePostActions()', TL_ERROR);
-						header('HTTP/1.1 400 Bad Request');
-						die('Bad Request');
-					}
-
-					$objRow = $this->Database->prepare("SELECT * FROM " . $dc->table . " WHERE id=?")
-											 ->execute($intId);
-
-					// The record does not exist
-					if ($objRow->numRows < 1)
-					{
-						$this->log('A record with the ID "' . $intId . '" does not exist in table "' . $dc->table . '"', 'Ajax executePostActions()', TL_ERROR);
-						header('HTTP/1.1 400 Bad Request');
-						die('Bad Request');
-					}
-				}
-
-				$varValue = \Input::post('value');
-				$strKey = ($this->strAction == 'reloadPagetree') ? 'pageTree' : 'fileTree';
-
-				// Convert the selected values
-				if ($varValue != '')
-				{
-					$varValue = trimsplit("\t", $varValue);
-
-					// Automatically add resources to the DBAFS
-					if ($strKey == 'fileTree')
-					{
-						foreach ($varValue as $k=>$v)
-						{
-							$varValue[$k] = \Dbafs::addResource($v)->id;
-						}
-					}
-
-					$varValue = serialize($varValue);
-				}
-
-				// Set the new value
-				if ($GLOBALS['TL_DCA'][$dc->table]['config']['dataContainer'] == 'File')
-				{
-					$GLOBALS['TL_CONFIG'][$strField] = $varValue;
-					$arrAttribs['activeRecord'] = null;
-				}
-				elseif ($this->Database->tableExists($dc->table))
-				{
-					$objRow->$strField = $varValue;
-					$arrAttribs['activeRecord'] = $objRow;
-				}
-
-				$arrAttribs['id'] = $strFieldName;
-				$arrAttribs['name'] = $strFieldName;
-				$arrAttribs['value'] = $varValue;
-				$arrAttribs['strTable'] = $dc->table;
-				$arrAttribs['strField'] = $strField;
-
-				$objWidget = new $GLOBALS['BE_FFL'][$strKey]($arrAttribs);
-				echo $objWidget->generate();
-				exit; break;
-
-			// Feature/unfeature an element
-			case 'toggleFeatured':
-				if (class_exists($dc->table, false))
-				{
-					$dca = new $dc->table();
-
-					if (method_exists($dca, 'toggleFeatured'))
-					{
-						$dca->toggleFeatured(\Input::post('id'), ((\Input::post('state') == 1) ? true : false));
-					}
-				}
-				exit; break;
-
-			// Toggle subpalettes
-			case 'toggleSubpalette':
-				$this->import('BackendUser', 'User');
-
-				// Check whether the field is a selector field and allowed for regular users (thanks to Fabian Mihailowitsch) (see #4427)
-				if (!is_array($GLOBALS['TL_DCA'][$dc->table]['palettes']['__selector__']) || !in_array($this->Input->post('field'), $GLOBALS['TL_DCA'][$dc->table]['palettes']['__selector__']) || ($GLOBALS['TL_DCA'][$dc->table]['fields'][$this->Input->post('field')]['exclude'] && !$this->User->hasAccess($dc->table . '::' . $this->Input->post('field'), 'alexf')))
-				{
-					$this->log('Field "' . $this->Input->post('field') . '" is not an allowed selector field (possible SQL injection attempt)', 'Ajax executePostActions()', TL_ERROR);
-					header('HTTP/1.1 400 Bad Request');
-					die('Bad Request');
-				}
-
-				if ($dc instanceof DC_Table)
-				{
-					if (\Input::get('act') == 'editAll')
-					{
-						$this->strAjaxId = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', \Input::post('id'));
-						$this->Database->prepare("UPDATE " . $dc->table . " SET " . \Input::post('field') . "='" . (intval(\Input::post('state') == 1) ? 1 : '') . "' WHERE id=?")->execute($this->strAjaxId);
-
-						if (\Input::post('load'))
-						{
-							echo $dc->editAll($this->strAjaxId, \Input::post('id'));
-						}
+						echo $objWidget->generateAjax(\Input::post('folder', true), \Input::post('field'), intval(\Input::post('level')));
 					}
 					else
 					{
-						$this->Database->prepare("UPDATE " . $dc->table . " SET " . \Input::post('field') . "='" . (intval(\Input::post('state') == 1) ? 1 : '') . "' WHERE id=?")->execute($dc->id);
+						echo $objWidget->generate();
+					}
+					exit; break;
+
+				// Reload the page/file picker
+				case 'reloadPagetree':
+				case 'reloadFiletree':
+					$intId = \Input::get('id');
+					$strField = $strFieldName = \Input::post('name');
+
+					// Handle the keys in "edit multiple" mode
+					if (\Input::get('act') == 'editAll')
+					{
+						$intId = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', $strField);
+						$strField = preg_replace('/(.*)_[0-9a-zA-Z]+$/', '$1', $strField);
+					}
+
+					// Validate the request data
+					if ($GLOBALS['TL_DCA'][$dc->table]['config']['dataContainer'] == 'File')
+					{
+						// The field does not exist
+						if (!array_key_exists($strField, $GLOBALS['TL_CONFIG']))
+						{
+							$this->log('Field "' . $strField . '" does not exist in the global configuration', 'Ajax executePostActions()', TL_ERROR);
+							header('HTTP/1.1 400 Bad Request');
+							die('Bad Request');
+						}
+					}
+					elseif ($this->Database->tableExists($dc->table))
+					{
+						// The field does not exist
+						if (!$this->Database->fieldExists($strField, $dc->table))
+						{
+							$this->log('Field "' . $strField . '" does not exist in table "' . $dc->table . '"', 'Ajax executePostActions()', TL_ERROR);
+							header('HTTP/1.1 400 Bad Request');
+							die('Bad Request');
+						}
+
+						$objRow = $this->Database->prepare("SELECT * FROM " . $dc->table . " WHERE id=?")
+												 ->execute($intId);
+
+						// The record does not exist
+						if ($objRow->numRows < 1)
+						{
+							$this->log('A record with the ID "' . $intId . '" does not exist in table "' . $dc->table . '"', 'Ajax executePostActions()', TL_ERROR);
+							header('HTTP/1.1 400 Bad Request');
+							die('Bad Request');
+						}
+					}
+
+					$varValue = \Input::post('value');
+					$strKey = ($this->strAction == 'reloadPagetree') ? 'pageTree' : 'fileTree';
+
+					// Convert the selected values
+					if ($varValue != '')
+					{
+						$varValue = trimsplit("\t", $varValue);
+
+						// Automatically add resources to the DBAFS
+						if ($strKey == 'fileTree')
+						{
+							foreach ($varValue as $k=>$v)
+							{
+								$varValue[$k] = \Dbafs::addResource($v)->id;
+							}
+						}
+
+						$varValue = serialize($varValue);
+					}
+
+					// Set the new value
+					if ($GLOBALS['TL_DCA'][$dc->table]['config']['dataContainer'] == 'File')
+					{
+						$GLOBALS['TL_CONFIG'][$strField] = $varValue;
+						$arrAttribs['activeRecord'] = null;
+					}
+					elseif ($this->Database->tableExists($dc->table))
+					{
+						$objRow->$strField = $varValue;
+						$arrAttribs['activeRecord'] = $objRow;
+					}
+
+					$arrAttribs['id'] = $strFieldName;
+					$arrAttribs['name'] = $strFieldName;
+					$arrAttribs['value'] = $varValue;
+					$arrAttribs['strTable'] = $dc->table;
+					$arrAttribs['strField'] = $strField;
+
+					$objWidget = new $GLOBALS['BE_FFL'][$strKey]($arrAttribs);
+					echo $objWidget->generate();
+					exit; break;
+
+				// Feature/unfeature an element
+				case 'toggleFeatured':
+					if (class_exists($dc->table, false))
+					{
+						$dca = new $dc->table();
+
+						if (method_exists($dca, 'toggleFeatured'))
+						{
+							$dca->toggleFeatured(\Input::post('id'), ((\Input::post('state') == 1) ? true : false));
+						}
+					}
+					exit; break;
+
+				// Toggle subpalettes
+				case 'toggleSubpalette':
+					$this->import('BackendUser', 'User');
+
+					// Check whether the field is a selector field and allowed for regular users (thanks to Fabian Mihailowitsch) (see #4427)
+					if (!is_array($GLOBALS['TL_DCA'][$dc->table]['palettes']['__selector__']) || !in_array($this->Input->post('field'), $GLOBALS['TL_DCA'][$dc->table]['palettes']['__selector__']) || ($GLOBALS['TL_DCA'][$dc->table]['fields'][$this->Input->post('field')]['exclude'] && !$this->User->hasAccess($dc->table . '::' . $this->Input->post('field'), 'alexf')))
+					{
+						$this->log('Field "' . $this->Input->post('field') . '" is not an allowed selector field (possible SQL injection attempt)', 'Ajax executePostActions()', TL_ERROR);
+						header('HTTP/1.1 400 Bad Request');
+						die('Bad Request');
+					}
+
+					if ($dc instanceof DC_Table)
+					{
+						if (\Input::get('act') == 'editAll')
+						{
+							$this->strAjaxId = preg_replace('/.*_([0-9a-zA-Z]+)$/', '$1', \Input::post('id'));
+							$this->Database->prepare("UPDATE " . $dc->table . " SET " . \Input::post('field') . "='" . (intval(\Input::post('state') == 1) ? 1 : '') . "' WHERE id=?")->execute($this->strAjaxId);
+
+							if (\Input::post('load'))
+							{
+								echo $dc->editAll($this->strAjaxId, \Input::post('id'));
+							}
+						}
+						else
+						{
+							$this->Database->prepare("UPDATE " . $dc->table . " SET " . \Input::post('field') . "='" . (intval(\Input::post('state') == 1) ? 1 : '') . "' WHERE id=?")->execute($dc->id);
+
+							if (\Input::post('load'))
+							{
+								echo $dc->edit(false, \Input::post('id'));
+							}
+						}
+					}
+					elseif ($dc instanceof \DC_File)
+					{
+						$val = (intval(\Input::post('state') == 1) ? true : false);
+						$this->Config->update("\$GLOBALS['TL_CONFIG']['".\Input::post('field')."']", $val);
 
 						if (\Input::post('load'))
 						{
+							$GLOBALS['TL_CONFIG'][\Input::post('field')] = $val;
 							echo $dc->edit(false, \Input::post('id'));
 						}
 					}
-				}
-				elseif ($dc instanceof \DC_File)
-				{
-					$val = (intval(\Input::post('state') == 1) ? true : false);
-					$this->Config->update("\$GLOBALS['TL_CONFIG']['".\Input::post('field')."']", $val);
-
-					if (\Input::post('load'))
-					{
-						$GLOBALS['TL_CONFIG'][\Input::post('field')] = $val;
-						echo $dc->edit(false, \Input::post('id'));
-					}
-				}
-				exit; break;
-
-			// HOOK: pass unknown actions to callback functions
-			default:
-				$this->executePostActionsHOOK($dc);
-				break;
+					exit; break;
+			}
 		}
-	}
 
-	protected function executePostActionsHOOK($dc)
-	{
 		if (isset($GLOBALS['TL_HOOKS']['executePostActions']) && is_array($GLOBALS['TL_HOOKS']['executePostActions']))
 		{
 			foreach ($GLOBALS['TL_HOOKS']['executePostActions'] as $callback)
